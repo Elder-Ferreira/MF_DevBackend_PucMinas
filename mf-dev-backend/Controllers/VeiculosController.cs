560e04e9bcfa5dc78e0a5c9f0b627ab79e4db94c
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-08-04 17:38:30 -0300

    Criando VeiculoController

---
+++ mf-dev-backend/Controllers/VeiculosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mf_dev_backend.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace mf_dev_backend.Controllers
+{
+
+    public class VeiculosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VeiculosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Veiculos.ToListAsync());
+        }
+    }
+}
+
+       
