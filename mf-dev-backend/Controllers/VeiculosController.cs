80c417f49d70106e5a2718c5a7f174da67724627
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-08-07 19:54:38 -0300

    Adicionando view Create

---
+++ mf-dev-backend/Controllers/VeiculosController.cs
@@ -0,0 +1,42 @@
+﻿using mf_dev_backend;
+using mf_dev_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Veiculos.ToListAsync();
+
+            return View(dados);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Veiculo veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Veiculos.Add(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(veiculo);
+        }
+
+    }
+}
